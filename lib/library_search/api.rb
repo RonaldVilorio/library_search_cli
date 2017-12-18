51b19368d7628dbd3d2197f813e0e33a2d53c694
ronald vilorio <soccer23415@gmail.com>
2017-12-18 22:58:55 +0000

    createlibraries method and initialize method bad code on Library

--- lib/library_search/api.rb
+++ lib/library_search/api.rb
@@ -1,13 +1,14 @@
 class LibrarySearch::API
   def self.get_libraries(zip_code)
     data = RestClient.get(
-    "http://www.worldcat.org/libraries/geosearch.xml?countryCode=US&postalCode=#{zip_code}
+    "http://www.worldcat.org/libraries/geosearch.json?countryCode=US&postalCode=#{zip_code}
     &wskey=Nbtgqc4PfUGgeYo1IwxXQ3zzkkNvlYRgCtVecBXcPCSTVErvxpCreKl5cLUzDB1gxIWpUPHRRajkOZ9W")
-    binding.pry
 
-    @parsed_data = JSON.parse(data)
-
-
+    @parsed_libraries = JSON.parse(data)
+    @parsed_libraries.each do |library|
+      LibrarySearch::Library.create_libraries(library)
+    end
+    
 
   end
 end
--- lib/library_search/library.rb
+++ lib/library_search/library.rb
@@ -1,2 +1,21 @@
 class LibrarySearch::Library
+  @@all = []
+
+  def self.create_libraries(library)
+    self.new(
+    library["name"],
+    library["address"],
+    library["phoneNumber"],
+    library["emails"],
+    )
+  end
+  def initialize(name=nil,emails=nil,phoneNumber=nil,address=nil)
+
+    @email = email
+    @phoneNumber = phoneNumber
+    @address = address
+    @name = name
+    binding.pry
+    @@all << self
+  end
 end
