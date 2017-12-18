2adabb06d1f62c1476893b009bfa888335c15be0
ronald vilorio <soccer23415@gmail.com>
2017-12-18 22:05:07 +0000

    receieved data and user zip/parsed data not working on API class

--- lib/library_search/api.rb
+++ lib/library_search/api.rb
@@ -1,5 +1,13 @@
 class LibrarySearch::API
-  def get_libraries
+  def self.get_libraries(zip_code)
+    data = RestClient.get(
+    "http://www.worldcat.org/libraries/geosearch.xml?countryCode=US&postalCode=#{zip_code}
+    &wskey=Nbtgqc4PfUGgeYo1IwxXQ3zzkkNvlYRgCtVecBXcPCSTVErvxpCreKl5cLUzDB1gxIWpUPHRRajkOZ9W")
+    binding.pry
+
+    @parsed_data = JSON.parse(data)
+
+
 
   end
 end
--- lib/library_search/cli.rb
+++ lib/library_search/cli.rb
@@ -1,5 +1,7 @@
 class LibrarySearch::CLI
   def call
-    puts "Welcome"
+    puts "Welcome please enter a zip code to find libraries near you."
+    zip = gets.chomp
+    LibrarySearch::API.get_libraries(zip)
   end
 end
