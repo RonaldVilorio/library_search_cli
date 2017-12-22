817d8b2b8a10b97dc7d0db3f642bc752cba64468
ronald vilorio <soccer23415@gmail.com>
2017-12-22 04:14:31 +0000

    added all my variables on library class

--- lib/library_search/library.rb
+++ lib/library_search/library.rb
@@ -2,15 +2,27 @@ class LibrarySearch::Library
   @@all = []
 
   def self.create_libraries(library)
+    binding.pry
     self.new(
     library["name"],
     library["address"]["stateName"],
+    library["address"]["street1"],
+    library["address"]["city"],
+    library["address"]["postalCode"],
+    library["address"]["countryName"],
+    library["phoneNumber"]
+
     )
   end
-  def initialize(name=nil,state=nil)
+  def initialize(name=nil,state=nil,street=nil,city=nil,zip=nil,country=nil,phone=nil)
     @name = name
     @state = state
-    binding.pry
+    @street = street
+    @city = city
+    @zip = zip
+    @country = country
+    @phone = phone
+    # binding.pry
     @@all << self
   end
 end
