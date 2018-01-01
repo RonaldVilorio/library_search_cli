c13880dedf58b0a1022e4f6a362f8546f9e4ec01
ronald vilorio <soccer23415@gmail.com>
2018-01-01 19:27:28 +0000

    added while loop to list for zip code/ msg on exiting program

--- lib/library_search/cli.rb
+++ lib/library_search/cli.rb
@@ -4,17 +4,24 @@ class LibrarySearch::CLI
   end
 
   def list
-    puts "Welcome, please enter a zip code to find libraries near you."
-    zip = gets.chomp
-    LibrarySearch::API.get_libraries(zip)
-    puts ""
-    puts "*** Libraries near you ***"
-    puts ""
-    LibrarySearch::Library.all.each.with_index(1) do |library,i|
-      puts "#{i}. #{library.name}"
+    zip = nil
+    while zip.to_s.size != 5
+      puts "Welcome, please enter a zip code to find libraries near you."
+      zip = gets.chomp
+      zip = zip.to_i
+      if zip.to_s.size != 5
+        puts "Enter a valid zip code that is 5 digits long."
+      else
+        LibrarySearch::API.get_libraries(zip)
+        puts ""
+        puts "*** Libraries near you ***"
+        puts ""
+        LibrarySearch::Library.all.each.with_index(1) do |library,i|
+          puts "#{i}. #{library.name}"
+        end
+        puts ""
+      end
     end
-    puts ""
-
   end
   def print_info(library)
     puts ""
@@ -38,6 +45,8 @@ class LibrarySearch::CLI
       user_input = gets.chomp
       if user_input == "list"
         list
+      elsif user_input == "exit"
+        puts "Have a great day!"
       elsif user_input.to_i == 0
         if library = LibrarySearch::Library.find_by_name(user_input)
           print_info(library) else puts "couldn't find a library named #{user_input}"
